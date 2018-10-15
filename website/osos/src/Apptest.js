6b34faa889acaf8d4bcf940315158a233e842b51
Ollie Smith <oliver.smith@uniform.net>
2018-10-15 15:09:41 +0100

    new font

--- website/osos/src/Apptest.js
+++ website/osos/src/Apptest.js
@@ -1,33 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Link, Route, Switch } from 'react-router-dom';
-import About from './About';
+// import About from './About';
 
 const Home = () => (
   <div>
     <h2>Home</h2>
   </div>
-)
-
-
+);
 
 const Work = () => (
   <div>
     <h2>Work</h2>
   </div>
-)
+);
 
 const Opinions = () => (
   <div>
     <h2>Opinions</h2>
   </div>
-)
+);
 
 const Trust = () => (
   <div>
     <h2>Trust</h2>
   </div>
-)
+);
 
 class Apptest extends Component {
   render() {
@@ -36,22 +34,30 @@ class Apptest extends Component {
         <nav className="navbar navbar-light">
           <ul className="nav navbar-nav">
             /* Link components are used for linking to other views */
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/About">About</Link></li>
-            <li><Link to="/Work">Work</Link></li>
-            <li><Link to="/Opinions">Opinions</Link></li>
-            <li><Link to="/Trust">Trust</Link></li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/About">About</Link>
+            </li>
+            <li>
+              <Link to="/Work">Work</Link>
+            </li>
+            <li>
+              <Link to="/Opinions">Opinions</Link>
+            </li>
+            <li>
+              <Link to="/Trust">Trust</Link>
+            </li>
           </ul>
         </nav>
 
         <switch>
-          /* Route components are rendered if the path prop matches the current URL */
-          <Route exact={true} path="/" component={Home}/>
-          /* use exact prop if you want a route to be rendered only if the paths are exactly the same */
-          <Route path="/About" component={About}/>
-          <Route path="/Work" component={Work}/>
-          <Route path="/Opinions" component={Opinions}/>
-          <Route path="/Trust" component={Trust}/>
+          /* Route components are rendered if the path prop matches the current
+          URL */
+          <Route exact={true} path="/" component={Home} />
+          /* use exact prop if you want a route to be rendered only if the paths
+          are exactly the same */
         </switch>
       </div>
     );
